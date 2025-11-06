c79152fa503b10114d1db377cd3e091ab480b9a5
Tikesh Sahu <tikeshs392@gmail.com>
2025-11-06 23:20:16 +0530

    Done some task

--- Netflix Clone/src/components/TitleCards/TitleCards.jsx
+++ Netflix Clone/src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,18 @@
-import React, { useRef, useEffect }  from 'react'
+import React, { useRef, useEffect, useState }  from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjJiOGViMDFiOGQwMzY3NzM4YzdlZjcwMjhiZjkxYSIsIm5iZiI6MTc2MjQ0NTM4NS41NTgsInN1YiI6IjY5MGNjODQ5ZTc2OTQzODJkN2Y2ZDU1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.araWQGaw8lGfX-7IUH8D8TUfVBaEgYCLxb9iix9Rvlk'
+    }
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -11,6 +20,11 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     const cardList = cardsRef.current;
     cardList.addEventListener('wheel', handleWheel);
   }, []) 
@@ -19,10 +33,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
