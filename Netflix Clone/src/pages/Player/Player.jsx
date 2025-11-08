3124a127686af0e649e305095c0bedcb9538e3a6
Tikesh Sahu <tikeshs392@gmail.com>
2025-11-08 21:50:04 +0530

    Done with the clone.

--- Netflix Clone/src/pages/Player/Player.jsx
+++ Netflix Clone/src/pages/Player/Player.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjJiOGViMDFiOGQwMzY3NzM4YzdlZjcwMjhiZjkxYSIsIm5iZiI6MTc2MjQ0NTM4NS41NTgsInN1YiI6IjY5MGNjODQ5ZTc2OTQzODJkN2Y2ZDU1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.araWQGaw8lGfX-7IUH8D8TUfVBaEgYCLxb9iix9Rvlk'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}} />
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameborder="0" allowFullScreen></iframe>
+      <div className='player-Info'>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
